a6651272b94b09053a74abec4ec148a70cb8a8ad
Hoang-Tien-Manh-Duc <ducdeveloper@1201@gmail.com>
2025-08-27 14:51:30 +0700

    feat: add model to project

---
+++ src/main/java/com/dennis/model/ContactInformation.java
@@ -0,0 +1,17 @@
+package com.dennis.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class ContactInformation {
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String instagram;
+}
